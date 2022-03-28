5ff667fd6609bfc9a58a0f77069f7ced4d906fbc
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-03-28 17:38:46 +0700

    fix: update LoG mask

--- src/laplacian_of_gaussian.m
+++ src/laplacian_of_gaussian.m
@@ -5,9 +5,14 @@ function value = laplacian_of_gaussian(sigma, mask_dim)
 
     left_boundary = -floor(mask_dim / 2);
     right_boundary = floor(mask_dim / 2);
-    [meshgrid_x, meshgrid_y] = meshgrid(linspace(left_boundary, right_boundary, mask_dim));
+    lin = round(linspace(left_boundary, right_boundary, mask_dim));
+
+    [meshgrid_x, meshgrid_y] = meshgrid(lin, lin);
+    % [meshgrid_x, meshgrid_y] = meshgrid(linspace(left_boundary, right_boundary, mask_dim));
 
     D = meshgrid_x.^2 + meshgrid_y.^2;
     value = ((D - 2 * (sigma^2)) ./ (sigma^4)) .* exp(-D ./ (2 * (sigma^2)));
+
+    h = surf(value);
 
 end
