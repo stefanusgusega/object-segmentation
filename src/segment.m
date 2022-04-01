0e12dfa8e7f63c75c666222229ff9f89c1bcf9c0
Stefanus Gusega Gunawan <gunawanstefanus006@gmail.com>
2022-04-01 14:47:48 +0700

    chore: remove debug

--- src/segment.m
+++ src/segment.m
@@ -10,13 +10,13 @@ function imgOut = segment(edgeIm, oriIm, technique)
     mask = imdilate(mask, strel('line', 3, 135));
 
     mask = imdilate(mask, strel('disk', 5));
-    
+
     if ~strcmp(technique, 'sobel') && ~strcmp(technique, 'prewitt')
-        disp(technique);
+        % disp(technique);
         mask = imfill(mask, 8, 'holes');
-        imshow(mask);
+        % imshow(mask);
     end
 
     % seg = imclose(imfill(edgeIm, 'holes'), strel('disk', 10));
-    imgOut = oriIm .* uint8(mask)
+    imgOut = oriIm .* uint8(mask);
 end
